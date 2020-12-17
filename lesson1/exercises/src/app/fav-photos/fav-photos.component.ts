3fd4aea87426e5c2600a927b17ef0b1cbc709448
Raissa Havens <raissahavens@gmail.com>
2020-12-16 21:41:10 -0600

    Updates on html and css files

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Nice shots!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.myfitnesspal.com%2Fwant-to-crush-your-goals-get-smart%2F&psig=AOvVaw2UoEVuGbzfGIdzID5E7Mh9&ust=1608262471482000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCNhN6K1O0CFQAAAAAdAAAAABAJ';
   image3 = '';
 
   constructor() { }
